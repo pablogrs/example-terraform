42896e419be8cd963dab9b24d604d4794ab5c90d
Pablo Sanchez <pablogrs@gmail.com>
2024-02-26 14:22:03 +0000

    Update infracost_test.tf

--- infracost_test.tf
+++ infracost_test.tf
@@ -22,6 +22,21 @@ resource "aws_instance" "my_web_app" {
   }
 }
 
+resource "aws_instance" "my_server_app" {
+  ami = "ami-005e54dee72cc1d00"
+
+  instance_type = "m3.xlarge" # <<<<<<<<<< Try changing this to m5.xlarge to compare the costs
+
+  tags = {
+    Environment = "production"
+    Service = "web-app"
+  }
+
+  root_block_device {
+    volume_size = 1000 # <<<<<<<<<< Try adding volume_type="gp3" to compare costs
+  }
+}
+
 resource "aws_lambda_function" "my_hello_world" {
   runtime = "nodejs12.x"
   handler = "exports.test"
